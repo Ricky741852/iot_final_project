4055551065ccd94fc8a895bb562002db52b4da21
Hunter Hsu <80113hunterhsu@gmail.com>
2022-12-10 16:10:15 +0800

    Update SetNodeId.ino
    used for node 4 reset

--- lora_mesh/SetNodeId/SetNodeId.ino
+++ lora_mesh/SetNodeId/SetNodeId.ino
@@ -2,8 +2,8 @@
 #include <EEPROM.h>
 
 // change this to be the ID of your node in the mesh network
-uint8_t nodeId = 3;
-uint8_t groupId = 2;
+uint8_t nodeId = 4;
+uint8_t groupId = 1;
 uint8_t memberNum = 4;
 
 void setup() {
