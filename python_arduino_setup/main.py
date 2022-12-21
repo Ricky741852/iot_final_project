83f6ab9265c6643058f0d82444a6e3ad4dff24d4
Hunter Hsu <80113hunterhsu@gmail.com>
2022-12-21 11:53:17 +0800

    new pyserial writer

--- python_arduino_setup/main.py
+++ python_arduino_setup/main.py
@@ -1,80 +1,120 @@
+# from Arduino import Arduino
+import serial
 import time
+from serial.tools import list_ports
 
-nodeId = input("nodeId: ")
-groupId = input("groupId: ")
-memberNum = input("memberNum: ")
+# get current device port number
+current_device = ""
+port = list(list_ports.comports())
+for p in port:
+    if ("/dev/cu.usb" in p.device):
+        print("device: " + p.device)
+        current_device = p.device
 
-back_nodeId = -1
-back_groupId = -1
-back_memberNum = -1
+data_set_status = 0
+
+nodeId = int(input("nodeId: "))
+groupId = int(input("groupId: "))
+memberNum = int(input("memberNum: "))
 
 key_nodeId = "nodeId-->"
 key_groupId = "groupId-->"
 key_memberNum = "memberNum-->"
 key_toPython = "toPython-->"
+
 # write data to arduino
-def write_data():
-    print((key_nodeId + nodeId).encode())
-    read_back()
-    print((key_groupId + groupId).encode())
-    read_back()
-    print((key_memberNum + memberNum).encode())
-    read_back()
-    time.sleep(2)
-    print("SUCCESS")
-    print("nodeId: " + nodeId)
-    print("groupId: " + groupId)
-    print("memberNum: " + memberNum)
+def write_data(select):
+    back_nodeId = 253
+    back_groupId = 253
+    back_memberNum = 253
+    if (select == 1):
+        print("nodeId: " + str(nodeId))
+        print("python: " + key_nodeId + str(nodeId))
+        print("waiting", end="")
+        while (back_nodeId != nodeId):
+            print(".", end="")
+            arduino.write((key_nodeId + str(nodeId)).encode())
+            back_nodeId = read_back(nodeId, key_nodeId)
+        print("<<< nodeId set! >>>\n")
+    elif (select == 2):
+        print("groupId: " + str(groupId))
+        print("python: " + key_groupId + str(groupId))
+        print("waiting", end="")
+        while (back_groupId != groupId):
+            print(".", end="")
+            arduino.write((key_groupId + str(groupId)).encode())
+            back_groupId = read_back(groupId, key_groupId)
+        print("<<< groupId set! >>>\n")
+    elif (select == 3):
+        print("memberNum: " + str(memberNum))
+        print("python: " + key_memberNum + str(memberNum))
+        print("waiting", end="")
+        while (back_memberNum != memberNum):
+            print(".", end="")
+            arduino.write((key_memberNum + str(memberNum)).encode())
+            back_memberNum = read_back(memberNum, key_memberNum)
+        print("<<< memberNum set! >>>\n")
+    elif (nodeId == back_nodeId) and (groupId == back_groupId) and (memberNum == back_memberNum):
+        data_set_status = 1
 
-def read_back():
+def read_back(origin, key):
     time.sleep(1)
-    # data = arduino.readline().decode('utf-8').rstrip()
-    # if (data):
-    #     if (key_toPython in data):
-    #         data = data.replace(key_toPython, "")
-    #         print(data)
-    #         if (key_nodeId in data):
-    #             back_nodeId = data.replace(key_nodeId, "")
-    #             if (not compare(nodeId, back_nodeId)):
-    #                 write_data()
-    #         elif (key_groupId in data):
-    #             back_groupId = data.replace(key_groupId, "")
-    #             if (not compare(groupId, back_groupId)):
-    #                 write_data()
-    #         elif (key_memberNum in data):
-    #             back_memberNum = data.replace(key_memberNum, "")
-    #             if (not compare(memberNum, back_memberNum)):
-    #                 write_data()
+    # print(str(origin) + ": " + str(back))
+    data = arduino.readline().decode('utf-8').rstrip()
+    if (data):
+        if (key_toPython in data):
+            data = data.replace(key_toPython, "")
+            print("arduino: " + data)
+            if (key in data):
+                back = data.replace(key, "")
+                print(str(origin) + ", " + back)
+                return int(back)
+                # if (not compare(origin, back)):
+                #     write_data()
 
-def compare(a, b):
-    if (a != b):
-        return 0
-    else:
-        return 1
+# def compare(a, b):
+#     if (a != b):
+#         return 0
+#     else:
+#         return 1
 
 # print(arduino)
 def read_serial():
     while(1):
         write_data()
-        # data = arduino.readline().decode('utf-8').rstrip()
-        # if (data):
-        #     print(data)
+        data = arduino.readline().decode('utf-8').rstrip()
+        if (data):
+            print(data)
             # if (key_toPython in data):
             #     print(data.replace(key_toPython, ""))
 
 # init board
-# arduino = serial.Serial(current_device, 115200, timeout=0.1)
+arduino = serial.Serial(current_device, 115200, timeout=0.1)
+
+def data_selector():
+    select = 0
+    while(data_set_status != 1):
+        print("1. nodeId\t 2. groupId\t3. memberNum")
+        select = input("please select: ")
+        if (select == "1"):
+            print("writing nodeID...")
+        elif (select == 2):
+            print("writing groupID...")
+        elif (select == 3):
+            print("writing memberNum...")
+        write_data(int(select))
 
 # arduino.write(bytes(("BasicData-->"), 'utf-8'))
-# while (1):
+while (1):
     # print("in")
-    # data = arduino.readline().decode('utf-8').rstrip()
-    # if ("StartDataWrite-->" in data): 
-if (1): 
-    print("device: /dev/cu.usbserial-1430")
-    print("Start Writing Data to Arduino...")
-    # read_serial()
-    write_data()
-    # else: 
-    #     if (data):
-    #         print(data)
+    data = arduino.readline().decode('utf-8').rstrip()
+    if ("StartDataWrite-->" in data): 
+    # if (1): 
+        print("Start Writing Data to Arduino...")
+        # read_serial()
+        data_selector()
+        # write_data()
+        print("SUCCESS")
+    else: 
+        if (data):
+            print(data)
